cac1146960990f095125408c9d43db80f770ae1c
Cargill Seiveright <microcargill@hotmail.com>
2020-05-27 05:09:25 -0500

    works: send encypted message to server

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 from socket import *
 import sys
 import nacl.utils
-from nacl.public import PrivateKey, Box
+from nacl.public import PrivateKey, Box, PublicKey
 
 args = sys.argv
 
@@ -20,13 +20,34 @@ serverPort = 5687
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverHost, serverPort))
 
-client_secretKey = PrivateKey.generate()
+#generate private and public keys
+privateKey = PrivateKey.generate()
+publicKey = privateKey.public_key
 
-message = "hello world!"
+#get public key in bytes
+send_publicKey = publicKey.__bytes__()
 
-clientSocket.send(message.encode())
+#sends over public key
+clientSocket.send(send_publicKey) #SEND
+print("sent public key")
 
-fromServer = clientSocket.recv(1024).decode('utf-8')
+#receives public key from server in bytes
+fromServer = clientSocket.recv(1024) #RECEIVE
+print("received public key")
 
-print(fromServer)
+#create public key object from server
+server_pubKey = PublicKey(fromServer)
+
+#create client box
+client_box = Box(privateKey, server_pubKey)
+
+message = "hello world"
+
+encrypted = client_box.encrypt(bytes(message,'utf-8'))
+
+#send encrypted message
+clientSocket.send(encrypted) #SEND
+print("sent encrypted message")
+
 clientSocket.close()
+print("closed connection")
