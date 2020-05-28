ddb932dbd23dea47ec19bf793491d672454f4cc7
Cargill Seiveright <microcargill@hotmail.com>
2020-05-28 01:47:59 -0500

    client modifications

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from socket import *
-import sys
-import nacl.utils
+import sys, os
+from keyM import saveKey, loadKey
 from nacl.public import PrivateKey, Box, PublicKey
 
 args = sys.argv
@@ -14,40 +14,98 @@ if len(args) == 2:
 if len(args) == 3:
     pass
 
-serverHost = '0.0.0.0'
-serverPort = 5687
+clientSocket = None
+client_box = None
+send_publicKey = None
+privateKey = None
+publicKey = None
+server_publicKey = None
+server_pubObj = None
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverHost, serverPort))
+def connectServer(serverHost='0.0.0.0', serverPort=5687):
+    global clientSocket
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverHost, serverPort))
 
-#generate private and public keys
-privateKey = PrivateKey.generate()
-publicKey = privateKey.public_key
+def getKeys(key_name='client_key'):
+    global privateKey
+    global publicKey
 
-#get public key in bytes
-send_publicKey = publicKey.__bytes__()
+    if os.path.exists((key_name + '.key')):
+        privateKey = loadKey(key_name)
+        publicKey = privateKey.public_key
+    else:
+        saveKey(key_name)
+        getKeys(key_name)
 
-#sends over public key
-clientSocket.send(send_publicKey) #SEND
-print("sent public key")
+def initServerConnect():
+    global clientSocket
+    global publicKey
+    global send_publicKey
+    global server_publicKey
+    global server_pubObj
 
-#receives public key from server in bytes
-fromServer = clientSocket.recv(1024) #RECEIVE
-print("received public key")
+    clientSocket.send("sending client key".encode()) #SEND
+    fromServer = clientSocket.recv(1024).decode('utf-8')
+    if fromServer == "send client key":
+        #get public key in bytes
+        send_publicKey = publicKey.__bytes__()
 
-#create public key object from server
-server_pubKey = PublicKey(fromServer)
+        #sends over public key
+        clientSocket.send(send_publicKey) #SEND
+        print("sent client public key")
 
-#create client box
-client_box = Box(privateKey, server_pubKey)
+        #receives public key from server in bytes
+        server_publicKey = clientSocket.recv(1024) #RECEIVE
+        #create public key object from server
+        server_pubObj = PublicKey(server_publicKey)
+        print("received server public key")
+    else:
+        print("public key was not sent")
 
-message = "hello world"
 
-encrypted = client_box.encrypt(bytes(message,'utf-8'))
+def createBox():
+    global client_box
+    global privateKey
+    global server_pubObj
 
-#send encrypted message
-clientSocket.send(encrypted) #SEND
-print("sent encrypted message")
+    #create client box
+    client_box = Box(privateKey, server_pubObj)
 
-clientSocket.close()
-print("closed connection")
+def sendMessage(message="hello world"):
+    global client_box
+    global clientSocket
+
+    encrypted = client_box.encrypt(bytes(message,'utf-8'))
+
+    #send encrypted message
+    clientSocket.send(encrypted) #SEND
+    print("sent encrypted message")
+
+def main():
+    global clientSocket
+    try: 
+        connectServer()
+        getKeys()
+        initServerConnect()
+        createBox()
+        while True:
+            message = input("Enter message to send to the server: ")
+            if message == "exit(0)":
+                print("closing client")
+                break
+
+            sendMessage(message)
+            print("message sent to server")
+            response = clientSocket.recv(1024).decode('utf-8')
+            print("server: ", response)
+
+    except Exception as e:
+        print("something went wrong:")
+        print(e)
+    finally:
+        clientSocket.close()
+        print("closed connection")
+
+if __name__ == "__main__":
+    main()
