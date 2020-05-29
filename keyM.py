6d0bc80547587f8d3073aefcf7ac9d8b57f4bc33
Cargill Seiveright <microcargill@hotmail.com>
2020-05-28 19:29:43 -0500

    change from using pickle to custom key saver and reader

--- keyM.py
+++ keyM.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from nacl.public import PrivateKey, Box, PublicKey
-import pickle
+
 
 def genKey():
     """Generates PrivateKey object using PyNaCl"""
@@ -9,19 +9,23 @@ def genKey():
     return privateKey
 
 def saveKey(filename):
-    """Saves PrivateKey object into a byte file of '.key' extention 
+    """Saves PrivateKey object into a byte file of '.pttk' extention 
     param:filename-> str
     eg: saveKey("private_key")
     return None"""
-    with open((filename + '.key'), 'wb') as private_key_file:
+    with open((filename + '.pttk'), 'wb') as private_key_file:
         private_key = genKey()
-        pickle.dump(private_key, private_key_file)
+        
+        to_file_private_key = private_key.__bytes__()
+        private_key_file.write(to_file_private_key)
 
 def loadKey(filename):
-    """Returns PrivateKey object from a byte file of '.key' extention 
+    """Returns PrivateKey object from a byte file of '.pttk' extention 
     param:filename-> str
     eg: loadKey("private_key")
     return PrivateKey object"""
-    with open((filename + '.key'), 'rb') as private_key_file:
-        private_key = pickle.load(private_key_file)
-        return private_key
+    with open((filename + '.pttk'), 'rb') as private_key_file:
+        private_key = private_key_file.read()
+
+        private_keyObj = PrivateKey(private_key)
+        return private_keyObj
